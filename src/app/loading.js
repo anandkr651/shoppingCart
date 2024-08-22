e4b8909dd393226f0b47ba919a05cdc817e13378
Anand Kumar <kumaranandstudent@gmail.com>
2024-08-22 16:45:19 +0530

    classname in place of class

--- src/app/loading.js
+++ src/app/loading.js
@@ -6,7 +6,7 @@ function Loading() {
       <div role="status">
         <svg
           aria-hidden="true"
-          class="inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -20,12 +20,12 @@ function Loading() {
             fill="currentFill"
           />
         </svg>
-        <span class="sr-only">Loading...</span>
+        <span className="sr-only">Loading...</span>
       </div>
       <div role="status">
         <svg
           aria-hidden="true"
-          class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -39,12 +39,12 @@ function Loading() {
             fill="currentFill"
           />
         </svg>
-        <span class="sr-only">Loading...</span>
+        <span className="sr-only">Loading...</span>
       </div>
       <div role="status">
         <svg
           aria-hidden="true"
-          class="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -58,12 +58,12 @@ function Loading() {
             fill="currentFill"
           />
         </svg>
-        <span class="sr-only">Loading...</span>
+        <span className="sr-only">Loading...</span>
       </div>
       <div role="status">
         <svg
           aria-hidden="true"
-          class="inline w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          className="inline w-12 h-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -77,7 +77,7 @@ function Loading() {
             fill="currentFill"
           />
         </svg>
-        <span class="sr-only">Loading...</span>
+        <span className="sr-only">Loading...</span>
       </div>
       <span className="text-5xl font-semibold">Processing...</span>
     </div>
