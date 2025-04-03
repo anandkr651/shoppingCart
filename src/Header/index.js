b98117c14acdb38119f10cc6859fdffdb9c75bdb
Anand Kumar <kumaranandstudent@gmail.com>
2025-04-03 14:24:50 +0530

    refactor Header component for improved layout and sticky positioning

--- src/Header/index.js
+++ src/Header/index.js
@@ -14,7 +14,8 @@ function Header({ getSession }) { //props comes from provider
   }
 
   return (
-    <header className="font-semibold lg:text-2xl text-lg flex  border-2 rounded-xl shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] lg:m-8 m-4 px-4 min-h-16 tracking-wide z-50">
+    <header className=" sticky left-0 right-0 top-0 bg-white bottom-0 font-semibold lg:text-2xl text-lg  border-2 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] min-h-16 tracking-wide z-50">
+      <div className="flex px-10 py-4">
       <div className="flex justify-between items-center flex-wrap w-full">
         <h1>Shopping Cart</h1>
       </div>
@@ -30,6 +31,7 @@ function Header({ getSession }) { //props comes from provider
         <form action={getSession?.user ? handleAuthSignOut : handleAuthSignIn}>
           <Button type="submit">{getSession?.user ? "Logout" : "Login"}</Button>
         </form>
+      </div>
       </div>
     </header>
   );
